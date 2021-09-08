17687827d3cc7fd6655268ac5201409295d0ee36
uziasferreirazup <uzias.ferreira@zup.com.br>
2021-09-08 15:47:39 -0300

    fix: passing context from beagle view to actions
    Signed-off-by: uziasferreirazup <uzias.ferreira@zup.com.br>

--- beagle/lib/src/beagle_widget.dart
+++ beagle/lib/src/beagle_widget.dart
@@ -18,7 +18,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:beagle/beagle.dart';
-import 'package:beagle/src/utils/build_context_utils.dart';
 import 'package:flutter/widgets.dart';
 
 import 'bridge_impl/beagle_view_js.dart';
@@ -91,7 +90,7 @@ class _BeagleWidget extends State<BeagleWidget> {
           action: action,
           view: view,
           element: element,
-          context: context.findBuildContextForWidgetKey(element.getId()),
+          context: context,
         );
       });
 
