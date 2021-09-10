05815efb77968f042ee109328ac1f39b7762a048
Hernand Azevedo <hernand.azevedo@zup.com.br>
2021-09-10 18:08:13 -0300

    :white_check_mark: Adds submit form action tests
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- beagle_components/test/beagle_simple_form_test.dart
+++ beagle_components/test/beagle_simple_form_test.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E pdf to INOVACAO SA
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 import 'package:beagle/beagle.dart';
 import 'package:beagle_components/beagle_components.dart';
